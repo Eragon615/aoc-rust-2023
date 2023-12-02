a9d911b1447522b4ca6d8bd7360b60279eeb3b11
Clayton Slaughter <dragonmasterclay@gmail.com>
2023-12-02 00:05:35 -0600

    day 1 part 2 complete

--- src/days/day1.rs
+++ src/days/day1.rs
@@ -1,4 +1,6 @@
 use crate::Application;
+use std::io::Read;
+use std::{collections::BTreeMap, io::Cursor};
 
 impl Application {
     pub fn day1(self) {
@@ -31,5 +33,51 @@ impl Application {
         println!("{}", answer);
     }
 
-    fn part2(self) {}
+    fn part2(self) {
+        let mut answer = 0;
+        for line in self.input {
+            // if it's stupid and it works, it's not stupid
+            let mut map: BTreeMap<u64, u32> = BTreeMap::new();
+            let mut line = line.clone();
+            for i in 0..line.len() {
+                // check for actual numbers first
+                if line.as_bytes()[0].is_ascii_digit() {
+                    map.insert(i as u64, line.as_bytes()[0] as u32 - 48);
+                }
+                // now check for all spellings of numbers
+                // I got lucky, I searched the input and only 'one' through 'nine' appear
+                // this would be much harder otherwise
+                if line.find("one") == Some(0) {
+                    map.insert(i as u64, 1);
+                } else if line.find("two") == Some(0) {
+                    map.insert(i as u64, 2);
+                } else if line.find("three") == Some(0) {
+                    map.insert(i as u64, 3);
+                } else if line.find("four") == Some(0) {
+                    map.insert(i as u64, 4);
+                } else if line.find("five") == Some(0) {
+                    map.insert(i as u64, 5);
+                } else if line.find("six") == Some(0) {
+                    map.insert(i as u64, 6);
+                } else if line.find("seven") == Some(0) {
+                    map.insert(i as u64, 7);
+                } else if line.find("eight") == Some(0) {
+                    map.insert(i as u64, 8);
+                } else if line.find("nine") == Some(0) {
+                    map.insert(i as u64, 9);
+                }
+                // remove the first letter from the string before continuing
+                line.remove(0);
+            }
+            let mut additive = 0;
+            if let Some(first) = map.first_entry() {
+                additive += 10 * first.get();
+            }
+            if let Some(last) = map.last_entry() {
+                additive += last.get();
+            }
+            answer += additive;
+        }
+        println!("{}", answer);
+    }
 }
