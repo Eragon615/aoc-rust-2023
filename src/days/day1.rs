b9a1a48d0d1b7872feadc99460d90158a65ee460
Clayton Slaughter <dragonmasterclay@gmail.com>
2023-12-01 20:19:04 -0600

    initial commit, setup and ready to code

---
+++ src/days/day1.rs
@@ -0,0 +1,8 @@
+use crate::Application;
+
+impl Application {
+    pub fn day1(self) {
+        println!("Welcome to day 1!");
+        println!("{:?}", self.input);
+    }
+}
---
+++ src/main.rs
@@ -0,0 +1,34 @@
+mod args;
+mod days;
+
+use args::Args;
+use clap::Parser;
+use std::{
+    fs::File,
+    io::{prelude::*, BufReader},
+};
+
+pub struct Application {
+    pub args: Args,
+    pub input: Vec<String>,
+}
+
+fn main() {
+    let app = setup();
+    match app.args.day {
+        1 => app.day1(),
+        _ => println!("It is not yet the time..."),
+    };
+}
+
+fn setup() -> Application {
+    let args = Args::parse();
+    let filename = format!("input{}.txt", &args.day);
+    let file = File::open(filename).expect("no such file");
+    let buf = BufReader::new(file);
+    let input: Vec<String> = buf
+        .lines()
+        .map(|l| l.expect("Could not parse line"))
+        .collect();
+    Application { args, input }
+}
