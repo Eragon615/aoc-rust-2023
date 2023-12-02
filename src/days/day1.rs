42572faa2d26215eab5c8c4293e14c9b0ac87ecb
Clayton Slaughter <dragonmasterclay@gmail.com>
2023-12-01 20:50:24 -0600

    day 1 part 1 complete

--- src/days/day1.rs
+++ src/days/day1.rs
@@ -2,7 +2,34 @@ use crate::Application;
 
 impl Application {
     pub fn day1(self) {
-        println!("Welcome to day 1!");
-        println!("{:?}", self.input);
+        if self.args.part == 1 {
+            self.part1();
+        } else {
+            self.part2();
+        }
     }
+
+    fn part1(self) {
+        let mut answer = 0;
+        for line in self.input {
+            let mut first = 0;
+            let mut last = 0;
+            for char in line.chars() {
+                if char.is_numeric() {
+                    if first == 0 {
+                        first = char as u32 - 48;
+                    } else {
+                        last = char as u32 - 48;
+                    }
+                }
+            }
+            if last == 0 {
+                last = first;
+            }
+            answer += (first * 10) + last;
+        }
+        println!("{}", answer);
+    }
+
+    fn part2(self) {}
 }
