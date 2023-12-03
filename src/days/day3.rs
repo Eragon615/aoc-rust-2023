940869c7bdddf68863daf3f2eb0b475f35ca05d3
Clayton Slaughter <dragonmasterclay@gmail.com>
2023-12-03 09:52:22 -0600

    forgot to save these files

---
+++ src/days/day3.rs
@@ -0,0 +1,15 @@
+use crate::Application;
+
+impl Application {
+    fn day3(self) {
+        if self.args.part == 1 {
+            self.d3p1();
+        } else {
+            self.d3p2();
+        }
+    }
+
+    fn d3p1(self) {}
+
+    fn d3p2(self) {}
+}
