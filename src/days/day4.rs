1aacf24738e74974fb96bb9534dd418127bd3691
Clayton Slaughter <clayton.thomas.slaughter@ibm.com>
2023-12-05 13:43:21 -0600

    Completes 4-2

--- src/days/day4.rs
+++ src/days/day4.rs
@@ -7,9 +7,11 @@ struct Cards {
 
 #[derive(Debug)]
 struct Card {
-    id: u64,
+    _id: u64,
     winning_nums: Vec<u64>,
     played_nums: Vec<u64>,
+    copies: usize,
+    wins: usize,
 }
 
 impl Application {
@@ -18,7 +20,7 @@ impl Application {
         if self.args.part == 1 {
             self.d4p1(cards);
         } else {
-            self.d4p2();
+            self.d4p2(cards);
         }
     }
 
@@ -45,7 +47,26 @@ impl Application {
         println!("{answer}");
     }
 
-    fn d4p2(self) {}
+    fn d4p2(self, cards: Cards) {
+        let mut cards: Vec<Card> = cards.cards.into();
+        let mut answer = 0;
+        for i in 0..cards.len() {
+            let mut wins = 0;
+            for winning_num in &cards[i].winning_nums {
+                for played_num in &cards[i].played_nums {
+                    if played_num == winning_num {
+                        wins += 1;
+                    }
+                }
+            }
+            cards[i].wins = wins;
+            for j in (i + 1)..(i + cards[i].wins + 1) {
+                cards[j].copies += 1 * cards[i].copies;
+            }
+            answer += cards[i].copies;
+        }
+        println!("{answer}")
+    }
 }
 
 fn generate_cards(input: &Vec<String>) -> Cards {
@@ -68,9 +89,11 @@ fn generate_cards(input: &Vec<String>) -> Cards {
             .map(|n| n.parse::<u64>().expect("Oops I broke it"))
             .collect::<Vec<u64>>();
         cards.push(Card {
-            id,
+            _id: id,
             winning_nums,
             played_nums,
+            copies: 1,
+            wins: 0,
         })
     }
     return Cards { cards };
